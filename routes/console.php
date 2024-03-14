548520abc2d2fc77adf9c8a1e5a831b3ae343c25
Aquiles <abrito393@gmail.com>
2024-03-14 01:05:46 -0400

    fix connections db

--- routes/console.php
+++ routes/console.php
@@ -2,14 +2,8 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use App\Console\Commands\GetRemoteData;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
-
-Artisan::command('get-eluter-data', function () {
-    $command = new GetRemoteData();
-    $command->handle();
-})->purpose('Get remota data')->everyMinute();
